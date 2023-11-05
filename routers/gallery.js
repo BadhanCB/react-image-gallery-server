3f1254ee6828330dce7f93ce160b38e77915aa67
BadhanCB <badhancsesastc112@gmail.com>
2023-11-06 01:18:09 +0600

    define patch api for updating image serial

--- routers/gallery.js
+++ routers/gallery.js
@@ -27,7 +27,9 @@ async function run() {
 }
 run().catch(console.dir);
 
+// router for /gallery
 router
+  //get api for getting all image data sort by id
   .get("/", async (req, res) => {
     try {
       const result = await collection.find({}).sort({ id: 1 }).toArray();
@@ -36,6 +38,11 @@ router
       res.status(400).send({ message: "Failed to get Data" });
     }
   })
+  /**
+   * api for receive image as file
+   * reduce size and convert to webp image with sharp package
+   * convert img to base 64 data for storing to the mongodb
+   */
   .post("/", async (req, res) => {
     try {
       const { name, data, size, mimetype } = req?.files?.file;
@@ -70,6 +77,31 @@ router
       res.status(500).send(error);
     }
   })
+  /**
+   * to update img serial by receiving new serial id
+   * update img serial with Bulk operations builder
+   */
+  .patch('/', async (req, res) => {
+    try {
+      const updatedData = req.body;
+
+      const bulk = collection.initializeOrderedBulkOp();
+      updatedData.forEach((data) => {
+        bulk.find({ _id: ObjectId(data._id) }).updateOne({ $set: { id: data.id } });
+      });
+      await bulk.execute();
+
+      res.status(202).send({ message: "Image SuccessFully Updated" });
+    } catch (error) {
+      res.status(500).send({ message: "Failed to delete" });
+    }
+  })
+  /**
+   * delete multiple data by receiving array of _id
+   * wrap them with mongoDB ObjectId class
+   * delete multiple data by using $in operator
+   * after deleting, update img serial with Bulk operations builder
+   */
   .delete("/", async (req, res) => {
     try {
       let imgIds = req.body;
@@ -81,7 +113,7 @@ router
       existingData.forEach((data, i) => {
         bulk.find({ _id: data._id }).updateOne({ $set: { id: i + 1 } });
       });
-      bulk.execute();
+      await bulk.execute();
 
       if (result.deletedCount) {
         res.status(202).send({ message: "Image Deleted SuccessFully" });
